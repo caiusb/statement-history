aab65a1bb631bff92081d7dea6e4284019d8dd87
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-09-19 15:18:40 -0700

    Stub main file

---
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/Main.scala
@@ -0,0 +1,9 @@
+package edu.oregonstate.mutation.statementHistory
+
+object Main {
+  def main(args: Array[String]) {
+    val repo = args(0)
+    val file = args(1)
+    val lineNumber = args(2)
+  }
+}
