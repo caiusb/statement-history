f8de15f1c0719ff6872bc7a0dd2ab7e7bb6e390f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-09-21 20:56:36 -0700

    Extracted method
    For readability

--- src/main/scala/edu/oregonstate/mutation/statementHistory/StatementFinder.scala
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/StatementFinder.scala
@@ -1,6 +1,7 @@
 package edu.oregonstate.mutation.statementHistory
 
-import org.eclipse.jdt.core.dom.Statement
+import org.eclipse.jdt.core.dom.{ASTNode, Statement}
+import org.eclipse.jdt.core.dom.AST._
 import org.gitective.core.CommitUtils
 import org.eclipse.jgit.lib.Repository
 import java.io.File
@@ -9,9 +10,9 @@ import org.gitective.core.BlobUtils
 import org.eclipse.core.runtime.NullProgressMonitor
 
 class StatementFinder(repo: String) {
-  
+
   val git = Git.open(new File(repo))
-  
+
   def getLines(string: String): Seq[LineInfo] = {
     import scala.collection.mutable._
     val lines = string.split("\n")
@@ -22,16 +23,11 @@ class StatementFinder(repo: String) {
       lineInfo
     } }
   }
-  
+
   def findStatement(commitSHA: String, file: String, lineNumber: Int): Statement = {
-    import org.eclipse.jdt.core.dom.AST._
-    import org.eclipse.jdt.core.dom.ASTParser._
-    
+
     val content = BlobUtils.getContent(git.getRepository, commitSHA, file)
-    val parser = newParser(K_COMPILATION_UNIT)
-    parser.setKind(JLS8)
-    parser.setSource(content.toCharArray)
-    val ast = parser.createAST(new NullProgressMonitor)
+    val ast: ASTNode = getAST(content)
     val lines = getLines(content)
     val visitor = new StatementVisitor(lines)
     ast.accept(visitor)
@@ -39,5 +35,14 @@ class StatementFinder(repo: String) {
       case Some(x) => return x
       case None => return null
     }
+  }
+
+  private def getAST(content: String): ASTNode = {
+    import org.eclipse.jdt.core.dom.ASTParser._
+    val parser = newParser(K_COMPILATION_UNIT)
+    parser.setKind(JLS8)
+    parser.setSource(content.toCharArray)
+    val ast = parser.createAST(new NullProgressMonitor)
+    ast
   }
 }
