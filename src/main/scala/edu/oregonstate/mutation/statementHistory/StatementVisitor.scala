8c5b4cdeb548b3a7ccc7cff192e9d8148e8f8e8b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-09-25 11:21:45 -0700

    Checming all types of statements

--- src/main/scala/edu/oregonstate/mutation/statementHistory/StatementVisitor.scala
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/StatementVisitor.scala
@@ -25,4 +25,59 @@ class StatementVisitor() extends ASTVisitor {
 
   override def visit(node: ConstructorInvocation): Boolean =
     visitStatement(node)
+
+  override def visit(node: ContinueStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: BreakStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: DoStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: EmptyStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: EnhancedForStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: ExpressionStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: ForStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: IfStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: LabeledStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: ReturnStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: SuperConstructorInvocation): Boolean =
+    visitStatement(node)
+
+  override def visit(node: SwitchStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: WhileStatement): Boolean =
+  visitStatement(node)
+
+  //not tested
+  override def visit(node: SwitchCase): Boolean =
+    visitStatement(node)
+
+  override def visit(node: TryStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: ThrowStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: SynchronizedStatement): Boolean =
+    visitStatement(node)
+
+  override def visit(node: TypeDeclarationStatement): Boolean =
+    visitStatement(node)
 }
--- src/test/scala/edu/oregonstate/mutation/statementHistory/StatementVisitorTest.scala
+++ src/test/scala/edu/oregonstate/mutation/statementHistory/StatementVisitorTest.scala
@@ -18,9 +18,16 @@ class StatementVisitorTest extends FlatSpec with Matchers with BeforeAndAfter {
 
   private def getStatementMap(stmt: String): Map[Int, Statement] = {
     val cu = putStatementInCU(stmt)
-    getAST(cu).accept(visitor)
+    val ast = getAST(cu)
+    ast.accept(visitor)
     val stmtMap = visitor.getStatementMap
     stmtMap
+  }
+
+  private def checkCU(stmt: String): Unit = {
+    val visitor = new StatementVisitor
+    getAST(stmt).accept(visitor)
+    assertStatement(visitor.getStatementMap)
   }
 
   def checkStatement(stmt: String): Unit =
@@ -34,6 +41,48 @@ class StatementVisitorTest extends FlatSpec with Matchers with BeforeAndAfter {
   it should "find a variable declaration" in
     checkStatement("int x=3;")
 
+  //it should "find an assert statement" in
+  //  checkStatement("assert true;")
+
   it should "find a constructor invocation" in
-    checkStatement("Object x = new Object();")
+    checkStatement("this();")
+
+  it should "find a continue statement" in
+    checkStatement("continue;")
+
+  it should "find a break statement" in
+    checkStatement("break;")
+
+  it should "find a do statement" in
+    checkStatement("do {\nint x=3;}\nwhile(true);")
+
+  it should "find a empty statement" in
+    checkStatement(";")
+
+  it should "find an enhanced for statement" in
+    checkStatement("for(Object x : new Array[2]){}")
+
+  it should "find a for statement" in
+    checkStatement("for(int i=0; i<10; i++){}")
+
+  it should "find an if statement" in
+    checkStatement("if(true}{;}")
+
+  it should "find a labeled statement" in
+    checkStatement("label: ;")
+
+  it should "find a return statement" in
+    checkCU("public class A {\npublic int m(){\nreturn 2;}}")
+
+  it should "find a super constructor invocation" in
+    checkCU("public class A {\npublic A(){\nsuper();}}")
+
+  it should "find a switch statement" in
+    checkStatement("switch(3){\ncase 3: ;}")
+
+  ignore should "find a switch case statement" in
+    checkCU("public class A {\npublic void m() { switch(3){\ncase 3: ;}}}")
+
+  it should "find a while statement" in
+    checkStatement("while(true){;}")
 }
