323926de88bec20cf7f1bf2d233598eeb8ee039f
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-09-25 17:07:13 -0700

    Main implementation

--- src/main/scala/edu/oregonstate/mutation/statementHistory/Main.scala
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/Main.scala
@@ -1,9 +1,20 @@
 package edu.oregonstate.mutation.statementHistory
 
+import java.io.File
+
 object Main {
   def main(args: Array[String]) {
     val repo = args(0)
-    val file = args(1)
-    val lineNumber = args(2)
+    var jsonFile = args(1)
+
+    val detector = new StatementChangeDetector(repo)
+    val mutants = JSONDecoder.decode(new File(jsonFile))
+
+    val result = mutants.map(mutant => {
+      mutant.getFileName + "," + mutant.getLineNumber + "," +
+        detector.findCommits(mutant.getFileName, mutant.getLineNumber).map(commit => commit + ", ")
+    })
+
+    print(result)
   }
 }
