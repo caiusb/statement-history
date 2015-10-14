9edd2f34682482a5d75171a9c2782e233d2e3a0c
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-10-14 12:28:26 -0700

    Added safety test

---
+++ src/test/scala/edu/oregonstate/mutation/statementHistory/ChangeInfoTest.scala
@@ -0,0 +1,19 @@
+package edu.oregonstate.mutation.statementHistory
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by caius on 10/14/15.
+ */
+class ChangeInfoTest extends FlatSpec with Matchers {
+
+  it should "merge correctly" in {
+    val firstCommitInfo = new CommitInfo("abc", "ADD")
+    val secondCommitInfo = new CommitInfo("def", "UPDATE")
+    val info = new ChangeInfo(17, Seq(firstCommitInfo))
+    val add = new ChangeInfo(27, Seq(secondCommitInfo))
+    val merged = info.merge(add)
+    merged.getLine should equal (27)
+    merged.getChangedCommits should equal (Seq(firstCommitInfo ,secondCommitInfo))
+  }
+}
