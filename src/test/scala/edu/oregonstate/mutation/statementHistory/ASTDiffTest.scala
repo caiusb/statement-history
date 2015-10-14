bed5d7094ef83f7016ba8e91ea5acb5c9a9f423e
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2015-10-14 12:38:34 -0700

    Added safety test for the AST Diff

---
+++ src/test/scala/edu/oregonstate/mutation/statementHistory/ASTDiffTest.scala
@@ -0,0 +1,23 @@
+package edu.oregonstate.mutation.statementHistory
+
+import fr.labri.gumtree.actions.model.Update
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by caius on 10/14/15.
+ */
+class ASTDiffTest extends FlatSpec with Matchers {
+
+  it should "correctly find the diff from String" in {
+    val (actions, _) = ASTDiff.getActions("public class A{}", "public class B{}")
+    actions should have size 1
+    actions(0) shouldBe an [Update]
+  }
+
+  it should "correcty find the diff from Trees" in {
+    val (actions, _) = ASTDiff.getActions(ASTDiff.getTree("public class A{}"), ASTDiff.getTree("public class B{}"))
+    actions should have size 1
+    actions(0) shouldBe an [Update]
+  }
+
+}
