2acd4352c7a5035de57ef6850100970d113e8004
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-11 22:58:12 -0800

    Added info about the blocks

--- src/main/scala/edu/oregonstate/mutation/statementHistory/StatementInfo.scala
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/StatementInfo.scala
@@ -1,18 +1,43 @@
 package edu.oregonstate.mutation.statementHistory
 
-import org.eclipse.jdt.core.dom.{CompilationUnit, ASTNode}
+import org.eclipse.jdt.core.dom._
 
 /**
  * Created by caius on 9/25/15.
  */
-class StatementInfo(private var fileName: String, private var lineNumber: Int, private var className: String){
+class StatementInfo(private var fileName: String, private var lineNumber: Int, private var className: String) {
 
-  def this(file:String, node: ASTNode) =
+  private var otherInfo = ""
+
+  def this(file: String, node: ASTNode) = {
     this(file, node.getRoot.asInstanceOf[CompilationUnit].getLineNumber(node.getStartPosition), "")
+    computeOtherInfo(node)
+  }
 
-  def getFileName:String = fileName
-  def getLineNumber:Int = lineNumber
-  def getClassName:String = className
+  def computeOtherInfo(node: ASTNode) = {
+    val root = node.getRoot.asInstanceOf[CompilationUnit]
+    otherInfo = node match {
+      case b: Block => val t = node.getParent match {
+        case _: IfStatement => "if("
+        case _: ForStatement => "for("
+        case _: EnhancedForStatement => "for("
+        case _: WhileStatement => "while("
+        case _: DoStatement => "do("
+        case _: MethodDeclaration => "method("
+        case _: TryStatement => "try("
+        case _: CatchClause => "catch("
+        case _ => "other("
+      }
+        t + root.getLineNumber(b.getStartPosition) + ":" + root.getLineNumber(b.getStartPosition + b.getLength) + "),"
+      case _ => ""
+    }
+  }
 
-  def printInfo: String = getFileName + "," + getLineNumber + ","
+  def getFileName: String = fileName
+
+  def getLineNumber: Int = lineNumber
+
+  def getClassName: String = className
+
+  def printInfo: String = getFileName + "," + getLineNumber + "," + otherInfo
 }
---
+++ src/test/scala/edu/oregonstate/mutation/statementHistory/StatementInfoTest.scala
@@ -0,0 +1,59 @@
+package edu.oregonstate.mutation.statementHistory
+
+import org.eclipse.jdt.core.dom.ASTNode
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.revwalk.RevCommit
+
+/**
+ * Created by caius on 2/11/16.
+ */
+class StatementInfoTest extends GitTest {
+
+  private val FILE_NAME = "A.java"
+
+  def makeClass(block: String): String = {
+    "public class A{\npublic void m(){\n" + block + "\n{\nint x=3;}}}"
+  }
+
+  def find(commit: RevCommit): ASTNode = {
+    BlockFinder.findNode(Git.open(repo), commit.getName, FILE_NAME, 4)
+  }
+
+  it should "have the correct info for a if block" in {
+    val node = find(add(FILE_NAME, makeClass("if(true)")))
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",4,if(4:5),")
+  }
+
+  it should "have the correct info for a while block" in {
+    val node = find(add(FILE_NAME, makeClass("while(true)")))
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",4,while(4:5),")
+  }
+
+  it should "have the correct info for a for block" in {
+    val node = find(add(FILE_NAME, makeClass("for(int i=0;i<10;i++)")))
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",4,for(4:5),")
+  }
+
+  it should "have the correct info for an enhanced for block" in {
+    val node = find(add(FILE_NAME, makeClass("for(String x:xes)")))
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",4,for(4:5),")
+  }
+
+  it should "have the correct info for a try block" in {
+    val node = find(add(FILE_NAME, "public class A{\npublic void m(){\ntry\n{\nint x=3;}catch(Exception e){}}}"))
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",4,try(4:5),")
+  }
+
+  it should "have the correct info for a catch block" in {
+    val commit = add(FILE_NAME, "public class A{\npublic void m(){\ntry\n{\nint x=3;}catch(Exception e){\nint y=3;}}}")
+    val node = BlockFinder.findNode(Git.open(repo), commit.getName, FILE_NAME, 5)
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",5,catch(5:6),")
+  }
+
+  it should "have the correct info for a method body" in {
+    val commit = add(FILE_NAME, "public class A{\npublic void m(){\nint x=3;}}}")
+    val node = BlockFinder.findNode(Git.open(repo), commit.getName, FILE_NAME, 3)
+    new StatementInfo(FILE_NAME, node).printInfo should equal (FILE_NAME + ",2,method(2:3),")
+  }
+
+}
