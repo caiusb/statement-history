462a65a61338c54003d7a6e90810015e00899340
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2016-02-22 13:32:56 -0800

    Decoding line by line
    So I don't bust the maximum string size. Apparently, it's
    a problem...

--- src/main/scala/edu/oregonstate/mutation/statementHistory/JSONDecoder.scala
+++ src/main/scala/edu/oregonstate/mutation/statementHistory/JSONDecoder.scala
@@ -3,7 +3,6 @@ package edu.oregonstate.mutation.statementHistory
 import java.io.File
 
 import org.eclipse.jdt.core.dom.ASTNode
-import org.eclipse.jgit.api.Git
 import play.api.libs.json.Json
 
 import scala.io.Source
@@ -11,31 +10,38 @@ import scala.io.Source
 object JSONDecoder {
 
   def decode(file: File) : Seq[StatementInfo] = {
-    decode(Source.fromFile(file).mkString)
+    Source.fromFile(file).getLines().map(decodeLine).toSeq
+  }
+
+  def decodeLine(line: String): StatementInfo = {
+    var parsed = Json.parse(line)
+    var file = (parsed \ "mutant" \ "filename").as[String]
+    var rawLineNo = parsed \ "mutant" \ "line"
+    var lineNo = rawLineNo.asOpt[Int] match {
+      case Some(x) => x
+      case _ => rawLineNo.as[String].toInt
+    }
+    var className = (parsed \ "mutant" \ "id" \ "location" \ "class").as[String]
+    return new StatementInfo(file, lineNo, className)
   }
 
   def decode(json: String) : Seq[StatementInfo] = {
-    def decodeLine(line: String): StatementInfo = {
-      var parsed = Json.parse(line)
-      var file = (parsed \ "mutant" \ "filename").as[String]
-      var rawLineNo = parsed \ "mutant" \ "line"
-      var lineNo = rawLineNo.asOpt[Int] match {
-        case Some(x) => x
-        case _ => rawLineNo.as[String].toInt
-      }
-      var className = (parsed \ "mutant" \ "id" \ "location" \ "class").as[String]
-      return new StatementInfo(file, lineNo, className)
-    }
     json.split("\n").map(line => {
       decodeLine(line)
     }).toSeq
   }
 
-  def decode(file: File, find: (String, Int) => ASTNode): Seq[StatementInfo] =
-    decode(Source.fromFile(file).mkString, find)
+  def decode(file: File, find: (String, Int) => ASTNode): Seq[StatementInfo] = {
+    val statements = Source.fromFile(file).getLines().map(decodeLine).toSeq
+    computeExtraInfo(find, statements)
+  }
 
   def decode(json: String, find: (String, Int) => ASTNode) : Seq[StatementInfo] = {
     val statements = decode(json)
+    computeExtraInfo(find, statements)
+  }
+
+  private def computeExtraInfo(find: (String, Int) => ASTNode, statements: Seq[StatementInfo]): Seq[StatementInfo] = {
     statements.foreach(s => {
       val node = find(s.getFileName, s.getLineNumber)
       s.computeOtherInfo(node)
